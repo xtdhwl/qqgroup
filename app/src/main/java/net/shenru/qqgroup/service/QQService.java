c03fccfd76cf8bfa0cb0677ad09f6f6b50bab03a
qilonglu <qilonglu@58btc.com>
2018-01-23 21:11:32 +0800

    初始化项目

---
+++ app/src/main/java/net/shenru/qqgroup/service/QQService.java
@@ -0,0 +1,20 @@
+package net.shenru.qqgroup.service;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by xtdhwl on 23/01/2018.
+ */
+
+public class QQService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
